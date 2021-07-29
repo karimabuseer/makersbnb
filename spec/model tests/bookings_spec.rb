8916bebfa944b1df3b2aff1888cb49a3bbb56d13
Sibel Eren <edaeren575@gmail.com>
2021-07-29 16:06:30 +0100

    added tests for .incoming_bookings + .outgoing_bookings

--- spec/model tests/bookings_spec.rb
+++ spec/model tests/bookings_spec.rb
@@ -63,22 +63,52 @@ describe Booking do
       expect(booking_id.confirmed).to be_nil
     end
   end
+
+  describe '.outgoing_bookings' do
+    it 'should return the bookings for specified user' do
+
+      user = User.create(
+        name: 'sen',
+        email: 'banana@bread.com', 
+        password: 'banana')
+
+      listing = Listing.create( 
+        name: "sens hotel", 
+        description: "castle", 
+        price: "100",
+        user_id: user.user_id)
+
+      booking_1 = Booking.create(
+        start_date: '2021-10-12', 
+        end_date: '2021-11-12', 
+        listing_id: listing.listing_id, 
+        user_id: user.user_id
+       )
+
+    end
 end
 
-#   describe '.outgoing_bookings' do
-#     it 'should return the bookings for specified user' do
+describe '.incoming_bookings' do
+  it 'should return the bookings for specified user' do
 
-#       listing = Listing.create( 
-#         name: "sens hotel", 
-#         description: "castle", 
-#         price: "100")
-      
-#       user = User.create(
-#         name: 'sen',
-#         email: 'banana@bread.com', 
-#         password: 'banana')
+    user = User.create(
+      name: 'sen',
+      email: 'banana@bread.com', 
+      password: 'banana')
 
-#       booking_1 = Booking.create(start_date: '2021-10-12', end_date: '2021-11-12', listing_id: listing['listing_id'], user_id: user['user_id'] )
+    listing = Listing.create( 
+      name: "sens hotel", 
+      description: "castle", 
+      price: "100",
+      user_id: user.user_id)
 
-#     end
-# end
+    booking_1 = Booking.create(
+      start_date: '2021-10-12', 
+      end_date: '2021-11-12', 
+      listing_id: listing.listing_id, 
+      user_id: user.user_id
+    )
+    end
+  end
+
+end

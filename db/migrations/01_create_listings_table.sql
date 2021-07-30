0f5041c1e97c786ca531107c8d16acce581db3f3
jamescondon <jecondon66@icloud.com>
2021-07-30 11:12:19 +0100

    Console and coloured background

--- db/migrations/01_create_listings_table.sql
+++ db/migrations/01_create_listings_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE listings(listing_id SERIAL PRIMARY KEY, name VARCHAR(40), discription VARCHAR(450), price INT, user_id INT);
+CREATE TABLE listings(listing_id SERIAL PRIMARY KEY, name VARCHAR(40), blurb VARCHAR(450), price INT, user_id INT);

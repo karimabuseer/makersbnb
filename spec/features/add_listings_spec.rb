b7a74f2e5d5a55a4876b3a9fc0ce80749759ba7a
Matt Firmston <mattfirmston@gmail.com>
2021-07-30 14:40:17 +0100

    user is redirected to log in page when not logged in and tries to create listing

--- spec/features/add_listings_spec.rb
+++ spec/features/add_listings_spec.rb
@@ -1,11 +1,21 @@
 feature 'add-listings' do
-  scenario 'user can add a listing' do
-    visit('/listings')
+  context 'when user is logged in' do
+    scenario 'user can add a listing' do
+      sign_up
+      visit('/listings')
+      click_button('Add listing')
 
-    click_button('Add listing')
-
-    expect(current_path).to eq('/listings/add')
-
+      expect(current_path).to eq('/listings/add')
+    end
   end
 
+  context 'when user is not logged in' do
+    scenario 'user is redirected to log in page when they try to add listing' do
+      visit('/listings')
+      click_button('Add listing')
+
+      expect(current_path).to eq('/log_in')
+      expect(page).to have_content('You must be logged in to do that.')
+    end
+  end
 end

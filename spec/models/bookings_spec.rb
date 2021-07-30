0a7ad38ecc5b98829e91b8931a8d4ba36c1604bc
Sibel Eren <edaeren575@gmail.com>
2021-07-30 12:12:25 +0100

    added accept/deny methods with tests

--- spec/models/bookings_spec.rb
+++ spec/models/bookings_spec.rb
@@ -142,4 +142,156 @@ describe '.incoming_bookings' do
     end
   end
 
+  describe '.reject_if_booked' do
+    it 'rejects a booking if listing has been booked for those dates' do
+      user = User.create(
+        name: 'sen',
+        email: 'banana@bread.com', 
+        password: 'banana'
+      )
+      listing = Listing.create( 
+        name: "sens hotel", 
+        description: "castle", 
+        price: "100",
+        user_id: user.user_id
+      )
+      booking_1 = Booking.create(
+        start_date: '2021-10-12', 
+        end_date: '2021-11-12', 
+        listing_id: listing.listing_id, 
+        user_id: user.user_id
+      )
+      booking_2 = Booking.create(
+        start_date: '2021-10-12', 
+        end_date: '2021-11-12',  
+        listing_id: listing.listing_id, 
+        user_id: user.user_id
+       )
+
+       Booking.reject_if_booked(listing_id: listing.listing_id, start_date: booking_1.start_date, end_date: booking_1.end_date, booking_id: booking_1.booking_id)
+       find_booking_2_by_id = Booking.find_by_booking_id(booking_id: booking_2.booking_id)
+
+       expect(booking_1.confirmed).to eq(nil)
+       expect(find_booking_2_by_id.confirmed).to eq("f")
+      end
+    end
+
+    describe 'accept_booking' do
+      it 'accepts booking' do
+        user = User.create(
+          name: 'sen',
+          email: 'banana@bread.com', 
+          password: 'banana'
+        )
+        listing = Listing.create( 
+          name: "sens hotel", 
+          description: "castle", 
+          price: "100",
+          user_id: user.user_id
+        )
+        booking_1 = Booking.create(
+          start_date: '2021-10-12', 
+          end_date: '2021-11-12', 
+          listing_id: listing.listing_id, 
+          user_id: user.user_id
+        )        
+        booking_2 = Booking.create(
+          start_date: '2021-11-02', 
+          end_date: '2021-12-02', 
+          listing_id: listing.listing_id, 
+          user_id: user.user_id
+         )      
+         
+        booking_1.accept_booking
+  
+        expect(booking_1.confirmed).to eq(true)
+        expect(booking_2.confirmed).to eq(nil)
+      end
+    end
+  
+    describe 'deny_booking' do
+      it 'denies booking' do
+        user = User.create(
+          name: 'sen',
+          email: 'banana@bread.com', 
+          password: 'banana'
+        )
+        listing = Listing.create( 
+          name: "sens hotel", 
+          description: "castle", 
+          price: "100",
+          user_id: user.user_id
+        )
+        booking_1 = Booking.create(
+          start_date: '2021-10-12', 
+          end_date: '2021-11-12', 
+          listing_id: listing.listing_id, 
+          user_id: user.user_id
+        )        
+        booking_2 = Booking.create(
+          start_date: '2021-11-02', 
+          end_date: '2021-12-02', 
+          listing_id: listing.listing_id, 
+          user_id: user.user_id
+         )      
+         
+        booking_1.deny_booking
+  
+        expect(booking_1.confirmed).to eq(false)
+        expect(booking_2.confirmed).to eq(nil)
+      end
+    end
+
+    describe '.existing_booking' do
+      it 'should return true if a confirmed booking exists for given dates and for listing' do
+        user = User.create(
+          name: 'sen',
+          email: 'banana@bread.com', 
+          password: 'banana'
+        )
+        listing = Listing.create( 
+          name: "sens hotel", 
+          description: "castle", 
+          price: "100",
+          user_id: user.user_id
+        )
+        booking = Booking.create(
+          start_date: '2021-10-12', 
+          end_date: '2021-11-12', 
+          listing_id: listing.listing_id, 
+          user_id: user.user_id
+        )        
+
+        booking.accept_booking
+
+        existing_booking = Booking.existing_booking(start_date: booking.start_date, end_date: booking.end_date, listing_id: booking.listing_id)
+  
+        expect(existing_booking).to eq(true)
+      end
+  
+      it 'should return false if a confirmed booking does not exist for given dates and for listing' do
+        user = User.create(
+          name: 'sen',
+          email: 'banana@bread.com', 
+          password: 'banana'
+        )
+        listing = Listing.create( 
+          name: "sens hotel", 
+          description: "castle", 
+          price: "100",
+          user_id: user.user_id
+        )
+        booking = Booking.create(
+          start_date: '2021-01-12', 
+          end_date: '2021-02-12', 
+          listing_id: listing.listing_id, 
+          user_id: user.user_id
+        )        
+
+        existing_booking = Booking.existing_booking(start_date: '2021-11-12', end_date: '2021-11-23', listing_id: listing.listing_id)
+  
+        expect(existing_booking).to eq(false)
+      end
+  end
+
 end
